6bdb321149aabbba8336b60d3b98a785a1aba849
Demetri Van Sickle <demetriv.s.7@gmail.com>
2025-02-08 10:08:53 -0800

    Begin AD2 sample script

---
+++ Code/ExampleScripts/analog_discovery_example.py
@@ -0,0 +1,10 @@
+'''
+This is a test file for the Analog Discovery
+'''
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../__VendorAPIs/Diligent')))
+
+import WF_SDK
+
+
